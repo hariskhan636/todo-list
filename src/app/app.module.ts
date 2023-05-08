256744d439f8c2263da5931ad336a02f2b45b5c4
haris.khan636 <harisjriv@gmail.com>
2023-05-08 17:24:49 +0500

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './main/app.component';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,RouterOutlet,
   ],
   providers: [],
   bootstrap: [AppComponent]
